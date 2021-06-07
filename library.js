dbaa9e17ad9f85eddc8345a06a3c1eadc0755945
Thi Nguyen <tttn.tn13@gmail.com>
2021-06-07 16:26:38 -0700

    update

--- library.js
+++ library.js
@@ -2,37 +2,39 @@ const books = document.getElementById('books');
 let myLibrary = [];
 
 class Book {
-    constructor(bookID,name,author,pages,read) {
+    constructor(bookID,name,author,pages,isRead) {
         this.bookID = bookID;
         this.name = name;
         this.author = author;
         this.pages = pages;
-        this.read = Boolean;
+        this.isRead = isRead;
     }
 }
 
+function book_summary(book) {
+    return `Book: ${book.name} <br/> Author: ${book.author} <br/> Number of pages: ${book.pages}` ;
+}
+
 function addBookToLibrary() {
-    let bookName = document.getElementById('bookName').value;
-    let bookAuthor = document.getElementById('bookAuthor').value;
-    let bookPages = document.getElementById('bookPages').value;
-    if ((bookName !== "") && (bookAuthor !== "") && (bookPages !== "")) {
-        let bookID = (new Date().getMilliseconds()).toString();
-        newBook = new Book(bookID,bookName,bookAuthor, bookPages );
+    const bookName = document.getElementById('bookName');
+    const bookAuthor = document.getElementById('bookAuthor');
+    const bookPages = document.getElementById('bookPages');
+   
+    if ((bookName.value !== "") && (bookAuthor.value !== "") && (bookPages.value !== "")) {
+        const bookID = (new Date().getMilliseconds()).toString();
+        newBook = new Book(bookID,bookName.value,bookAuthor.value, bookPages.value );
         myLibrary.push(newBook);
         console.log(myLibrary);
-        localStorage.setItem("myLibraryStorage", JSON.stringify(myLibrary));
-        setup_view(newBook);
-        clearBookInfo();
+        persistToStorage();
     } else {
         alert("Fill all fields");
     }
 }
 
-
 function configureViews() {
-    let submitButton = document.querySelector('#submitButton');
+    const submitButton = document.querySelector('#submitButton');
     submitButton.addEventListener('click', addBookToLibrary);
-    let clearButton = document.querySelector('#clearButton');
+    const clearButton = document.querySelector('#clearButton');
     clearButton.addEventListener('click', clearBookInfo);
 }
 
@@ -42,50 +44,48 @@ function clearBookInfo() {
     document.getElementById('bookPages').value = "";
 }
 
-function book_summary(book) {
-    return `Book: ${book.name} <br/> Author: ${book.author} <br/> Number of pages: ${book.pages}` ;
-}
-
 function setup_view(book) {
-    let div = document.createElement('div');
+    const div = document.createElement('div');
     div.className = 'book';
     div.innerHTML = book_summary(book);
     div.id = book.bookID;
-    let div_buttons = document.createElement('div');
+    const div_buttons = document.createElement('div');
     div_buttons.className = 'div_buttons';
     
-    div_buttons.appendChild(create_delete_button(book, myLibrary));
+    div_buttons.appendChild(create_delete_button(book));
     div_buttons.appendChild(create_read_component(book));
     div.appendChild(div_buttons);
     books.appendChild(div);
 }
 
 function create_read_component(book) {
-    let div = document.createElement('div');
-
+    const div = document.createElement('div');
     div.className = "readButtons"
-
     div.appendChild(create_read_label())
     div.appendChild(create_read_toggle(book));
     return div;
 }
 
 function create_read_label() {
-    let p = document.createElement('p');
+    const p = document.createElement('p');
     p.innerHTML = 'Read';
-
     return p;
 }
 
 function create_read_toggle(book) {
-    let label = document.createElement('label');
+    const bookIndex = myLibrary.indexOf(book);
+    console.log(bookIndex)
+    console.log(myLibrary)
+    
+    const label = document.createElement('label');
     label.className = 'readSwitch';
 
-    let span = document.createElement('span');
+    const span = document.createElement('span');
     span.className = 'slider';
 
-    let checkbox = document.createElement('input');
+    const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
+    checkbox.checked = myLibrary[bookIndex].isRead;
 
     checkbox.addEventListener('change', () => {
         check_read(book, checkbox.checked);
@@ -97,35 +97,41 @@ function create_read_toggle(book) {
     return label;
 }
 
-function create_delete_button(book, book_library) {
-    let deleteButton = document.createElement('button');
+function create_delete_button(book) {
+    const deleteButton = document.createElement('button');
     deleteButton.className = "deleteButtons"
     deleteButton.textContent = 'Delete';
     deleteButton.addEventListener('click', () => {
-        book_library = book_library.filter(item => item.bookID !== book.bookID);
-        localStorage.setItem('myLibraryStorage', JSON.stringify(book_library)); 
         document.getElementById(book.bookID).remove();
+        const bookIndex = myLibrary.indexOf(book);
+        myLibrary.splice(bookIndex,1);
+        persistToStorage()
+        console.log(myLibrary)
     })
     return deleteButton;
 }
 
 function check_read(book, isRead) {
-    console.log("Changing state: " + isRead)
     book.isRead = isRead;
+    persistToStorage()
 }
 
 function load_from_storage() {
-    myLibraryFromStorage = JSON.parse(localStorage.getItem("myLibraryStorage"));
+    let myLibraryFromStorage = JSON.parse(localStorage.getItem("myLibraryStorage"));
     if (myLibraryFromStorage !== null) {
         myLibrary = myLibraryFromStorage;
     } else{
         myLibrary = [];
     }
+    myLibrary.forEach((book) => {
+        setup_view(book);
+        clearBookInfo();
+    })
+}
 
-    myLibrary.forEach(setup_view);
+function persistToStorage() {
+    localStorage.setItem("myLibraryStorage", JSON.stringify(myLibrary));
 }
 
 load_from_storage()
 configureViews()
-clearBookInfo()
-
